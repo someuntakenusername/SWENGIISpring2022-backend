24609f7a0c1f33b479f1f6f3f692e7cbb3e07267
SamBeyerMM <75656515+SamBeyerMM@users.noreply.github.com>
2022-04-21 00:22:42 -0500

    cors changes

--- src/main/java/com/example/userGuide/Constroller/ReviewController.java
+++ src/main/java/com/example/userGuide/Constroller/ReviewController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("review/")
 public class ReviewController {
