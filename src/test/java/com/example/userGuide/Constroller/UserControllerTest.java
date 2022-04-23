18de81c72a9439df14e8cb470d9721b4deef5ab6
Will Henderson <willcurtish@gmx.com>
2022-04-22 21:09:40 -0500

    UserController tests

---
+++ src/test/java/com/example/userGuide/Constroller/UserControllerTest.java
@@ -0,0 +1,31 @@
+package com.example.userGuide.Constroller;
+
+import com.example.userGuide.Service.UserService;
+import com.example.userGuide.model.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UserControllerTest {
+    @Autowired
+    private UserService userService;
+
+    @Test
+    void getUsers() {
+        assertThat(userService.getUsers()).isNotNull();
+    }
+
+    @Test
+    void updateAdmission() {
+        assertThat(userService.createUser(new User("john", "doe", "iamthechowderman@hotmail.org", "rosebud")).equals(new User("john", "doe", "iamthechowderman@hotmail.org", "rosebud")));
+    }
+
+    @Test
+    void searchByLastName() {
+        assertThat(userService.userByLastname("doe").equals(new User("john", "doe", "iamthechowderman@hotmail.org", "rosebud")));
+    }
+}
