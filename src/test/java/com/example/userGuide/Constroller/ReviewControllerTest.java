e7eafc393d85e9af4ac3ffc214fe8a39919c1882
donovanorellana <Donovan_Orellana1@baylor.edu>
2022-05-02 17:16:57 -0500

    Update ReviewControllerTest.java

--- src/test/java/com/example/userGuide/Constroller/ReviewControllerTest.java
+++ src/test/java/com/example/userGuide/Constroller/ReviewControllerTest.java
@@ -5,7 +5,9 @@ import com.example.userGuide.model.Location;
 import com.example.userGuide.model.Review;
 import com.example.userGuide.model.User;
 import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -14,18 +16,28 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 @SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ReviewControllerTest {
     @Autowired
     private ReviewService reviewService;
     private Location l = new Location();
 
-    @Before
+    
+    @BeforeAll
     public void setTestLocation() {
         l.setId("TestLocation");
     }
 
+
+    /*
+    @BeforeAll
+    public void setupTests() {
+        l.setId("TestLocation");
+        reviewService.createReview(new Review("TestLocation", "This is a test review"));
+    }
+    */
     @Test
-    void createReview() {
+    public void createReview() {
         assertThat(
                 reviewService.createReview(new Review("TestLocation", "This is a test review"))
                 .equals(new User("john", "doe", "iamthechowderman@hotmail.org", "rosebud"))
@@ -33,10 +45,10 @@ public class ReviewControllerTest {
     }
 
     @Test
-     void getReviews() {
+     public void getReviews() {
         reviewService.createReview(new Review("TestLocation", "This is a test review"));
         assertThat(
-                reviewService.getReviews("testLocationId").get(0).equals(new Review("TestLocation", "This is a test review"))
+                reviewService.getReviews("TestLocation").get(0).equals(new Review("TestLocation", "This is a test review"))
         );
     }
 
