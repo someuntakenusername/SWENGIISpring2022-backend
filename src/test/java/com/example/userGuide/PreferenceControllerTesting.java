b6166f3501beedb98f6197559ec1eb2082d78521
Maiqi Hou <70655340+HLeoF@users.noreply.github.com>
2022-05-03 19:44:15 -0500

    Update Preference Controller Integration Testing

--- src/test/java/com/example/userGuide/PreferenceControllerTesting.java
+++ src/test/java/com/example/userGuide/PreferenceControllerTesting.java
@@ -18,6 +18,7 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -49,16 +50,17 @@ public class PreferenceControllerTesting {
     }
 
     @Test
-    public void testRetrieveWithMockMVC() throws Exception {
+    public void testRetrievePreferenceWithMockMVC() throws Exception {
         String json = "{\"id\":\"1\",\"cost\":\"2\",\"rating\":\"3\",\"reviews\":\"great\",\"contact\":\"111\",\"city\":\"Waco\"}";
         this.mockMvc.perform(MockMvcRequestBuilders.post("/preference/createpreference").contentType(MediaType.APPLICATION_JSON)
                         .content(json)).andExpect(status().is2xxSuccessful());
-        this.mockMvc.perform(MockMvcRequestBuilders.get("/preference/1").contentType(MediaType.APPLICATION_JSON).content(json))
-                .andExpect(MockMvcResultMatchers.status().isOk());
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/preference/1").contentType(MediaType.APPLICATION_JSON)
+                .content(json)).andExpect(MockMvcResultMatchers.status().isOk());;
         RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/preference/1").accept(MediaType.APPLICATION_JSON);
         MvcResult result = mockMvc.perform(requestBuilder).andReturn();
         MockHttpServletResponse res = result.getResponse();
         assert(result.getResponse().getContentLength() >= 0);
+
     }
 }
 
